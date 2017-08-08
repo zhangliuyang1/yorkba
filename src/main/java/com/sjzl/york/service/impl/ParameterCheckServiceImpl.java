c65a328d776796bebda6ba9ddcdc7e5cdf8d8ac8
zhangliuyang1 <945294148@qq.com>
2017-08-08 20:25:41 +0800

    添加打印日志

--- src/main/java/com/sjzl/york/service/impl/ParameterCheckServiceImpl.java
+++ src/main/java/com/sjzl/york/service/impl/ParameterCheckServiceImpl.java
@@ -1,10 +1,14 @@
 package com.sjzl.york.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.sjzl.york.common.model.SysVerifyCode;
 import com.sjzl.york.util.MD5Util;
 import com.sjzl.york.service.IParameterCheckService;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -15,12 +19,15 @@ import java.util.Map;
 @Service("parameterCheckService")
 public class ParameterCheckServiceImpl implements IParameterCheckService {
 
+    private Logger logger = Logger.getLogger(ParameterCheckServiceImpl.class);
     private final static String SIGN_STRING = "sign";
 
     public boolean checkApplicationParameters(Map<String, String> params) {
 
         String calSing = calSign(params);
         String sign = params.get(SIGN_STRING);
+        logger.info("签名参数:"+ JSON.toJSON(params).toString());
+        logger.info("签名sign值"+sign);
 
         return sign != null && calSing.equalsIgnoreCase(sign);
     }
@@ -57,5 +64,26 @@ public class ParameterCheckServiceImpl implements IParameterCheckService {
         String paramString = sb.toString();
 
         return MD5Util.string2MD5(paramString);
+    }
+
+    public static void main(String[] args) {
+        /*app_id=yorkba&build=1&devicemodel=iPhone&
+        nonce=25099&sysname=iOS&sysversion=10.3&timestamp=1502114374
+        &udid=3CC74E62-B131-4FCC-B5B4-C2643E092237&version=1.0&
+*/
+        Map<String,String> map = new HashMap<>();
+        map.put("app_id","yorkba");
+        map.put("build","1");
+        map.put("devicemodel","iPhone");
+        map.put("nonce","25099");
+        map.put("sysname","iOS");
+        map.put("sysversion","10.3");
+        map.put("timestamp","1502114374");
+        map.put("udid","3CC74E62-B131-4FCC-B5B4-C2643E092237");
+        map.put("version","1.0");
+        ParameterCheckServiceImpl parameterCheckService = new ParameterCheckServiceImpl();
+        String res = parameterCheckService.calSign(map);
+
+        System.out.println(res);
     }
 }
